e7f04660cb35c5356938538515feb38a0fc0b309
Torsten Friebe <torsten.friebe@gmail.com>
2016-06-25 22:25:24 +0200

    Added helloWorld quick start project

---
+++ README.md
@@ -0,0 +1,1 @@
+mvn archetype:generate -DgroupId=net.gfu.seminar.ci -DartifactId=helloWorld -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
---
+++ helloWorld/src/main/java/net/gfu/seminar/ci/App.java
@@ -0,0 +1,13 @@
+package net.gfu.seminar.ci;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
